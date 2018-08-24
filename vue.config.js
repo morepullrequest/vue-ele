dca4341eef73be2ee46449e85a121024c415cbc0
Roger <rogeryonglucky@gmail.com>
2018-08-24 23:39:13 +0800

    mock

---
+++ vue.config.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+var apiRoutes = express.Router();
+app.use("/api", apiRoutes);
+
+var appData = require("./src/data/data.json");
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+module.exports = {
+  devServer: {
+    before(app) {
+      app.get("/api/seller", (req, res) => {
+        res.json({
+          errno: 0,
+          data: seller
+        });
+      });
+
+      app.get("/api/goods", (req, res) => {
+        res.json({
+          errno: 0,
+          data: goods
+        });
+      });
+
+      app.get("/api/ratings", (req, res) => {
+        res.json({
+          errno: 0,
+          data: ratings
+        });
+      });
+    }
+  }
+};
